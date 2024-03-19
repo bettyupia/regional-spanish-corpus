822ad4ba19109f1f6a9787aebba363fe314c7498
Kanakanajm <kanakanajm@outlook.com>
2024-03-19 13:26:21 -0500

    Update transcriber script to use whisper model directly instead of OpenAI API

--- transcriber/main.py
+++ transcriber/main.py
@@ -1,39 +1,39 @@
-from openai import OpenAI
+import whisper
+# from dotenv import load_dotenv
 import os
 import csv
 
+# Load .env
+# load_dotenv()
+
 # Detect audio files
-audio_folder = os.environ.get("AUDIO_FILES_PATH")
-if not os.path.exists():
+audio_folder = os.getenv("AUDIO_FILES_PATH")
+if not os.path.exists(audio_folder):
     raise Exception(f"The folder '{audio_folder}' does not exist.")
 
 # Filter .wav files
 audio_files = [
     file
     for file in os.listdir(audio_folder)
-    if file.endswith(f'.{os.environ.get("AUDIO_FILE_FORMAT")}')
+    if file.endswith(f'.{os.getenv("AUDIO_FILE_FORMAT")}')
 ]
 
 print(f"Found {len(audio_files)} audio files in the audio folder")
 
 # Transcribe audio files
-client = OpenAI()
 transcriptions = []
+model = whisper.load_model("medium", download_root=".cache/whisper")
 for audio_file_name in audio_files:
-    print(f"Transcribing {audio_file_name}")
-    with open(os.path.join(audio_folder, audio_file_name) , "rb") as audio_file:
-        transcription = client.audio.transcriptions.create(
-          model="whisper-1", 
-          file=audio_file
-        )
-        transcriptions.append({
-            "id": audio_file_name,
-            "text": transcription.text
-        })
+    print("Transcribing", audio_file_name)
+    result = model.transcribe(os.path.join(audio_folder, audio_file_name), fp16=False)
+    transcriptions.append({
+        "id": audio_file_name,
+        "text": result["text"]
+    })
 
 # Save transcriptions as csv
 print("Saving")
-with open(os.environ.get("OUTPUT_FILE_PATH"), 'w') as csvfile:
+with open(os.getenv("OUTPUT_FILE_PATH"), 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=["id", "text"])
         writer.writeheader()
         writer.writerows(transcriptions)
